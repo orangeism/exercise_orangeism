113853d1e237d7ce94ba442cb58485a0c224105b
orangeism <1341771579@qq.com>
2024-08-12 16:23:00 +0800

    提交题目2

---
+++ java_base/src/题目二/test.java
@@ -0,0 +1,10 @@
+package 题目二;
+
+public class test {
+    public static void main(String[] args) {
+        GirlFriend gf=new GirlFriend("凤姐",155.0,130.0);
+        gf.show();
+        gf.cook();
+        gf.wash();
+    }
+}
